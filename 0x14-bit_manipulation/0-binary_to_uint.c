238b3b3c7e0a3924d17aaf12f551a6c5d93a5e8e
natholiR <nathiabera@gmail.com>
2023-08-03 10:17:22 -0700

    Bit-manipulation pro

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * binary_to_uint - Binary number to an unsigned int.
+ *
+ * @b: Binary number that is contained by string pointer.
+ *
+ * Return: Converted unsigned int number or if b is NULL it will be 0 or
+ *         char that are not '0' or '1'.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int result = 0;
+
+if (b == NULL)
+return (0);
+
+while (*b)
+{
+if (*b == '0' || *b == '1')
+{
+result <<= 1;
+result += (*b - '0');
+b++;
+}
+else
+{
+return (0);
+}
+}
+
+return (result);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+The 0x14-bit_manipulation files
